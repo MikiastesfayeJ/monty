f849d846b728f35dce26e5bec8abed20d1543fed
MikiastesfayeJ <57168613+MikiastesfayeJ@users.noreply.github.com>
2022-06-16 23:17:03 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,128 +1,104 @@
 #ifndef MONTY_H
 #define MONTY_H
-#include <fcntl.h>
+
+#define  _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
 #include <unistd.h>
-
-#ifndef TRUE
-#define TRUE 1
-#endif
-#ifndef FALSE
-#define FALSE 0
-#endif
-
-/**
- * enum Date_Format_Modes - The data format modes for this program.
- * @DF_LIFO: The data format code for a LIFO structure (like a stack)
- * @DF_FIFO: The data format code for a FIFO structure (like a queue)
- */
-enum Date_Format_Modes
-{
-	/* The data format code for a LIFO structure (like a stack) */
-	DF_LIFO,
-	/* The data format code for a FIFO structure (like a queue) */
-	DF_FIFO
-};
+#include <string.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: The integer stored at the node
- * @prev: A pointer to the previous element of the stack (or queue)
- * @next: A Pointer to the next element of the stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-	/* The integer stored at the node */
 	int n;
-	/* A pointer to the previous element of the stack (or queue) */
 	struct stack_s *prev;
-	/* A Pointer to the next element of the stack (or queue) */
 	struct stack_s *next;
 } stack_t;
 
 /**
- * struct instruction_s - Represents an opcode and its function
- * @opcode: The opcode name
- * @f: The function to handle the opcode
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-	/* The opcode name */
 	char *opcode;
-	/* The function to handle the opcode */
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* monty.c functions */
+/**
+ * struct stack_t_utils - global variables
+ * @instruction: string pointer to current instruction
+ * @dt_type: string to the name of current data structure in use
+ * @fp: pointer to the opened file
+ *
+ * Description: file pointer and current instruction being accessed
+ */
+typedef struct stack_t_utils
+{
+	char *instruction;
+	char *dt_type;
+	FILE *fp;
+} utils;
 
-char *get_data_mode(void);
-char **get_lines(void);
-void exit_program(int status);
-void clean_up_program(void);
+/* global variable */
+extern utils s_utils;
+utils s_utils;
 
-/* Opcode handlers */
+/* error handling prototypes*/
+void print_stderr(char *s);
+void clean_exit(void);
 
-void mty_op_push(stack_t **stack, unsigned int line_number);
-void mty_op_pall(stack_t **stack, unsigned int line_number);
-void mty_op_pint(stack_t **stack, unsigned int line_number);
-void mty_op_pop(stack_t **stack, unsigned int line_number);
-void mty_op_swap(stack_t **stack, unsigned int line_number);
-void mty_op_add(stack_t **stack, unsigned int line_number);
-void mty_op_nop(stack_t **stack, unsigned int line_number);
-void mty_op_sub(stack_t **stack, unsigned int line_number);
-void mty_op_div(stack_t **stack, unsigned int line_number);
-void mty_op_mul(stack_t **stack, unsigned int line_number);
-void mty_op_mod(stack_t **stack, unsigned int line_number);
-void mty_op_pchar(stack_t **stack, unsigned int line_number);
-void mty_op_pstr(stack_t **stack, unsigned int line_number);
-void mty_op_rotl(stack_t **stack, unsigned int line_number);
-void mty_op_rotr(stack_t **stack, unsigned int line_number);
-void mty_op_stack(stack_t **stack, unsigned int line_number);
-void mty_op_queue(stack_t **stack, unsigned int line_number);
+/* lists manipulating handling prototypes*/
+int print_stack(stack_t *head);
+stack_t *add_node_head(stack_t **head, int n);
+stack_t *add_node_end(stack_t **head, int n);
+int remove_node_start(stack_t **head, stack_t *current_node);
+void clear_stack(stack_t *head);
+int stack_length(stack_t *head);
 
-/* Reading helpers */
+/* file handling functions*/
+void open_file(char *filename);
+void read_file(void);
+void close_file(void);
+void handle_file(char *filename);
 
-char **read_file(char *path, int *lines_count);
-char *read_word(char *str, int *offset);
+/* instructions interpratation prototypes */
+void process_instructions(unsigned int *line_number, stack_t **stack);
+void (*get_instruction(char *cmd))(stack_t **stack, unsigned int line_number);
 
-/* Executor */
+/* instructions prototypes*/
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void div2(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void comments(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+void stack(stack_t **stack, unsigned int line_number);
+void queue(stack_t **stack, unsigned int line_number);
 
-instruction_t *get_opcode_handlers(void);
-void execute_line(char *line, int line_num, stack_t **stack_values);
+/* string handling prototypes*/
+int is_numeric(char *s);
 
-/* Stack helpers */
-
-void push(stack_t **stack, int n);
-int pop(stack_t **stack);
-void enqueue(stack_t **stack, int n);
-stack_t *get_top_element(stack_t **stack);
-stack_t *get_bottom_element(stack_t **stack);
-
-/* Memory helpers */
-
-void malloc_failure(char failed);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void mem_set(char *str, int n, char c);
-
-/* String utilities */
-
-int str_len(const char *str);
-char *str_cat(char *left, char *right, char can_free);
-char **str_split(char *str, char c, int *len_out, char can_free);
-
-/* Data validators */
-
-char is_integer(char *str);
-char is_ascii_char(int c);
-char is_whitespace(char c);
-
-#endif
+#endif /* MONTY_H */
